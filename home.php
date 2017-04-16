6ca537d7cef460d0befe8ac31bf5a28c3ac32be4
FranzouilleWazaah <franz.alliod@viacesi.fr>
2017-04-16 14:45:50 +0200

    Modif
    Modif

--- home.php
+++ home.php
@@ -13,8 +13,8 @@ require_once("session.php");
 	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
-<!DOCTYPE html>
-<html>
+<<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width">
